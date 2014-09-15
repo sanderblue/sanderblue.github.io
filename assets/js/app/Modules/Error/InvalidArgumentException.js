561c2ba8fb4ac0032cdc0d0b96d7dbcd761d0138
Sander Blue <william.a.blue@gmail.com>
2014-09-14 20:11:07 -0500

    I hope this doesn't break anything

--- assets/js/app/Modules/Error/InvalidArgumentException.js
+++
@@ -1,9 +0,0 @@
-// Work in progress
-function InvalidArgumentException(argument, method) {
-    this.message = 'Invalid argument ' + argument + ' passed to method ' + method;
-    this.name = 'InvalidArgumentException';
-
-    this.toString = function() {
-        return this.name + this.message
-    };
-}
